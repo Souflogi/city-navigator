3da9072561bbc7deb04bf12e3c8a74c48c557fc9
soufiane <sammahsoufiane01@gmail.com>
2024-07-04 13:58:41 +0100

    first commit

---
+++ src/pages/Pricing.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function Pricing() {
+  return (
+    <div className="">
+      <PageNav />
+      <h1>Pricing</h1>
+    </div>
+  );
+}
+
+export default Pricing;
